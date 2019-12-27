97cb17b71ded0578ce92d50370dbeb78ce5ada00
Eliott Gray <eliott.gray@mapbox.com>
2019-12-27 15:11:35 -0800

    Exit command changed from [shift+q] to [ctrl+shift+q]. Font changed from ‘Bangers’ to ‘Arvo.’ Font size of ‘Key’ div changed from 60vw to 30vw. Modifier keys (e.g. Shift, Ctrl) are no longer displayed. Keys are now colored based on unicode number.

--- renderer.js
+++ renderer.js
@@ -1,8 +1,18 @@
-Mousetrap.bind('shift+q', function() {
-    window.closeCurrentWindow();
+Mousetrap.bind('ctrl+shift+q', function() {
+    window.close();
 });
+
+colorMap = {0: "red", 1: "orange", 2: "gold", 3: "green", 4: "blue", 5: "indigo", 6: "violet"}
+
 document.body.addEventListener('keydown', function(e) {
     e.preventDefault();
-    document.getElementById("key").innerHTML = e.key;
+    if (e.key.length < 2){
+        charCode = e.key.charCodeAt(0);
+        document.getElementById("key").innerHTML = e.key;
+        color = colorMap[charCode % 7];
+        document.getElementById("key").style.color = color;
+    } else { 
+    	// Possibly handle non-character-keys differently.
+    }
     return false;
 });
