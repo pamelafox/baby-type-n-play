9c59ee2fbb153dde6d9d85507398b8762fa67800
Eliott Gray <eliott.gray@mapbox.com>
2019-12-27 23:35:46 -0800

    Fixing another variable without explicit 'const.'

--- renderer.js
+++ renderer.js
@@ -7,8 +7,8 @@ var colorMap = {0: 'red', 1: 'orange', 2: 'gold', 3: 'green', 4: 'blue', 5: 'ind
 document.body.addEventListener('keydown', function(e) {
     e.preventDefault();
     if (e.key.length < 2){
-        charCode = e.key.charCodeAt(0);
         document.getElementById('key').innerHTML = e.key;
+	const charCode = e.key.charCodeAt(0);
         const color = colorMap[charCode % 7];
         document.getElementById('key').style.color = color;
     }
