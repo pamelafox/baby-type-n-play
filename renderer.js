197127239bbb43304fd90b9595845cabf9f328c8
Eliott Gray <eliott.gray@mapbox.com>
2019-12-27 23:31:59 -0800

    Changed double quotes to singles. Using 'var' and 'const' explicitly. Removing foreer todo.

--- renderer.js
+++ renderer.js
@@ -2,17 +2,15 @@ Mousetrap.bind('ctrl+shift+q', function() {
     window.closeCurrentWindow();
 });
 
-colorMap = {0: "red", 1: "orange", 2: "gold", 3: "green", 4: "blue", 5: "indigo", 6: "violet"}
+var colorMap = {0: 'red', 1: 'orange', 2: 'gold', 3: 'green', 4: 'blue', 5: 'indigo', 6: 'violet'}
 
 document.body.addEventListener('keydown', function(e) {
     e.preventDefault();
     if (e.key.length < 2){
         charCode = e.key.charCodeAt(0);
-        document.getElementById("key").innerHTML = e.key;
-        color = colorMap[charCode % 7];
-        document.getElementById("key").style.color = color;
-    } else { 
-    	// Possibly handle non-character-keys differently.
+        document.getElementById('key').innerHTML = e.key;
+        const color = colorMap[charCode % 7];
+        document.getElementById('key').style.color = color;
     }
     return false;
 });
