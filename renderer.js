81565d21ab6cfd01a8fa22bef686e8eaa4cb876b
Eliott Gray <eliott.gray@mapbox.com>
2020-01-02 18:00:48 -0800

    Adding tone.js via CDN. Playing notes based on character code. Adding 400ms delay.

--- renderer.js
+++ renderer.js
@@ -2,15 +2,32 @@ Mousetrap.bind('ctrl+shift+q', function() {
     window.closeCurrentWindow();
 });
 
+const minimumDelayMilliseconds = 400;
+var latestKeyDownTime = new Date().getTime();
 const colorMap = {0: 'red', 1: 'orange', 2: 'gold', 3: 'green', 4: 'blue', 5: 'indigo', 6: 'violet'}
+
+//create a synth and connect it to the master output (your speakers)
+const synth = new Tone.Synth().toMaster();
+synth.oscillator.type = 'sine';
+const toneDuration = '16n'; // Note length described by musical notation.
+
 
 document.body.addEventListener('keydown', function(e) {
     e.preventDefault();
-    if (e.key.length < 2){
-        document.getElementById('key').innerHTML = e.key;
+    const currentTime = new Date().getTime();
+    const isAfterDelay = (currentTime - latestKeyDownTime) > minimumDelayMilliseconds;
+    if (isAfterDelay && (e.key.length == 1) && (e.repeat != true)){
+        latestKeyDownTime = currentTime;
         const charCode = e.key.charCodeAt(0);
+
+        // Update HTML.
         const color = colorMap[charCode % 7];
         document.getElementById('key').style.color = color;
+        document.getElementById('key').innerHTML = e.key;
+
+        // Play tone.
+        const frequency = (charCode - 45) * 10;
+        synth.triggerAttackRelease(frequency, toneDuration);
     }
     return false;
 });
