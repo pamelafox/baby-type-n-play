8f64a7f602831fcd553fdbbe33c2307efc2a4313
Eliott Gray <eliott.gray@mapbox.com>
2020-01-04 23:14:25 -0800

    Updating renderer to play tones.

--- renderer.js
+++ renderer.js
@@ -2,6 +2,8 @@ Mousetrap.bind('ctrl+shift+q', function() {
   window.closeCurrentWindow();
 });
 
+const delayMilliseconds = 400;
+let latestKeyDownTime = new Date().getTime();
 const colorMap = {
   0: 'red',
   1: 'orange',
@@ -12,13 +14,41 @@ const colorMap = {
   6: 'violet',
 };
 
+// create a synth and connect it to the master output (your speakers)
+const synth = new Tone.Synth().toMaster();
+synth.oscillator.type = 'sine';
+const toneDuration = '16n'; // Note length described by musical notation.
+
+
 document.body.addEventListener('keydown', function(e) {
   e.preventDefault();
-  if (e.key.length < 2) {
-    document.getElementById('key').innerHTML = e.key;
+  const currentTime = new Date().getTime();
+  const isAfterDelay = (currentTime - latestKeyDownTime) > delayMilliseconds;
+  if (isAfterDelay && (e.key.length == 1) && (e.repeat != true)) {
+    latestKeyDownTime = currentTime;
     const charCode = e.key.charCodeAt(0);
-    const color = colorMap[charCode % 7];
-    document.getElementById('key').style.color = color;
+    updateDOM(e.key, charCode);
+    playTone(charCode);
   }
   return false;
 });
+
+/**
+ * Update DOM with most-recently-pressed key.
+ * @param {string} key The pressed key.
+ * @param {number} charCode Character code of the pressed key.
+ */
+function updateDOM(key, charCode) {
+  const color = colorMap[charCode % 7];
+  document.getElementById('key').style.color = color;
+  document.getElementById('key').innerHTML = key;
+}
+
+/**
+ * Play a tone based on the pressed key.
+ * @param {number} charCode Character code of the pressed key.
+ */
+function playTone(charCode) {
+  const frequency = (charCode - 25) * 10;
+  synth.triggerAttackRelease(frequency, toneDuration);
+}
